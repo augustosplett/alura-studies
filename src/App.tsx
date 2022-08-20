a955cc152c6e41066d9203db70df61bdd6c325b2
Augusto Madeira Splett <augusto.splett@gmail.com>
2022-08-19 23:21:05 -0300

    implementando formulário em react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Formulario from './coponents/formulario';
+
+function App() {
+  return (
+    <div className="App">
+      teste
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
