c0b80053c8f9d428934de6e478b5f47f20cc9817
Augusto Madeira Splett <augusto.splett@gmail.com>
2022-08-22 23:00:03 -0300

    Tornando o texto do botão dinâmico.

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import style from './Botao.module.scss'
 
-class Botao extends React.Component{
+class Botao extends React.Component<{children: string }> {
     render(){
       return(
-        <button className={style.botao}> Botão </button>
+        <button className={style.botao}> 
+          {this.props.children}
+        </button>
       ) 
     }
 }
--- src/components/formulario/index.tsx
+++ src/components/formulario/index.tsx
@@ -32,7 +32,8 @@ class Formulario extends React.Component{
                     required
                 />
             </div>
-            <Botao />
+            <Botao>Adicionar</Botao> 
+
         </form>
       ) 
     }
