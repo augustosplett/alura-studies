71b64e52bab2abad45b08afafd1e71ce2982f897
Augusto Madeira Splett <augusto.splett@gmail.com>
2022-08-22 22:25:57 -0300

    transformando o CSS em módulos CSS

--- src/components/lista/index.tsx
+++ src/components/lista/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import './style.scss'
+import style from './Lista.module.scss'
 
 function Lista(){
 
     const tarefas = [{tarefa: 'React', tempo: '02:00:00' },{tarefa: 'JavaScript', tempo: '01:00:00' }]
 
     return(
-        <aside>
+        <aside className={style.listaTarefas}>
           <h2>Estudo do Dia</h2>  
           <ul>
             {tarefas.map((item, index) => (
-                <li key={index}>
+                <li key={index} className={style.item}>
                     <h3>{item.tarefa}</h3>
                     <span>{item.tempo}</span>
                 </li>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,10 @@
 import Formulario from '../components/formulario';
 import Lista from '../components/lista';
-import './style.scss'
+import style from './App.module.scss'
 
 function App() {
   return (
-    <div className="AppStyle">
+    <div className={style.AppStyle}>
       <Formulario />
       <Lista />
     </div>
